dec749e50472501f781cc31a5e9b4289b11db4be
Edwin Martinez <kastille84@gmail.com>
2017-12-21 14:12:29 -0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,19 @@ app.post('/todos', (req, res) => {
 	});
 });
 
+app.get('/todos', (req, res) => {
+	Todo.find().then(
+		(todos) => {
+			res.send( {
+				todos: todos
+			});
+		},
+		(e) => {
+			res.status(400).send(e);
+		}
+	);
+});
+
 app.listen(3000, () => {
 	console.log('Started Server on port 3000');
 });
