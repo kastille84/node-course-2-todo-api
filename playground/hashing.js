82595a4e1050f5c1e9cdab9d825cf3f47d9eee02
Edwin Martinez <kastille84@gmail.com>
2017-12-26 18:31:15 -0500

    Add POST /users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,39 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+	id: 10
+};
+
+var token = jwt.sign(data, '123abc');
+console.log(token);
+
+var decoded = jwt.verify(token, '123abc');
+console.log('decoded:', decoded); 
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
+
+// console.log(`message: ${message}`);
+// console.log(`hash: ${hash}`);
+
+// var data = {
+// 	username: "kastille84@gmail.com",
+// 	password: "123abc!"
+// };
+
+
+// var token = {
+// 	data,
+// 	hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// };
+
+// // token.data.username= "kasteolke";
+// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+
+// if (resultHash === token.hash) {
+// 	console.log('they match, Data was not maniuplated: ', token.hash);
+// } else {
+// 	console.log("Data was changed, Dont trust!");
+// }
--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,64 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const __ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
 	email: {
 		type: String,
 		required: true,
 		minlength: 1,
-		trim: true
-	}
+		trim: true,
+		unique: true,
+		validate: {
+			validator: (value)=> {
+				return validator.isEmail(value);
+			},
+			message: '{VALUE} is not a valid email'
+		}
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 6
+	},
+	tokens: [{
+		access: {
+			type: String,
+			required: true
+		},
+		token: {
+			type: String,
+			required: true
+		}
+	}]
 });
+
+UserSchema.methods.toJSON = function () {
+	// determines what exactly gets sent back when a model is converted to JSON values
+	var user = this;
+			// takes mongoose data and converts it into a regular object
+	var userObject = user.toObject();
+	return __.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+	var user = this;
+	var access = 'auth';
+	var token = jwt.sign({_id: user._id.toHexString(), access: access}, 'abc123').toString();
+
+	user.tokens.push({
+		access,
+		token
+	});
+
+	return user.save().then( () => {
+		return token;
+	});
+
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {
 	User
