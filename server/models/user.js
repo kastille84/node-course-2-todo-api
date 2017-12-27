1558c3d6e3a7e2f24210609b03a2d146d3312055
Edwin Martinez <kastille84@gmail.com>
2017-12-27 12:08:48 -0500

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const __ = require('lodash');
+var bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
 	email: {
@@ -74,6 +75,22 @@ UserSchema.statics.findByToken = function(token) {
 		'tokens.access': 'auth'
 	});
 };
+
+UserSchema.pre('save', function(next) {
+	var user = this;
+
+	if(user.isModified('password')){
+		//hash the password
+		bcrypt.genSalt(10, (err, salt) => {			
+			bcrypt.hash(user.password, salt, (err, hash)=> {
+				user.password = hash;
+				next();
+			});
+		});
+	} else {
+		next();
+	}
+});
 
 var User = mongoose.model('User', UserSchema);
 
